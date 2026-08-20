a8a2ca3f19a42de867cd92f73450fd993b5a359e
menukahansda <menukahansda199@gmail.com>
2026-08-20 17:51:54 +0530

    test: update fetch repo tests

--- backend/tests/test_fetch_repo.py
+++ backend/tests/test_fetch_repo.py
@@ -1,14 +1,16 @@
 from app.ingest.fetch_repo import validate_url
 
 EXPECTED = {
-    "target_repo_url": "https://github.com/menukahansda/ask-my-repo.git",
-    "repo_slug": "menukahansda-ask-my-repo",
+    "target_repo_url": "https://github.com/menukahansda/task-management-system.git",
+    "repo_slug": "menukahansda-task-management-system",
 }
 
 
 def test_extra_path_segments():
     assert (
-        validate_url("https://github.com/menukahansda/ask-my-repo/commits/main/")
+        validate_url(
+            "https://github.com/menukahansda/task-management-system/commits/main/"
+        )
         == EXPECTED
     )
 
@@ -18,18 +20,22 @@ def test_owner_only_trailing_slash():
 
 
 def test_clean_url():
-    assert validate_url("https://github.com/menukahansda/ask-my-repo") == EXPECTED
+    assert (
+        validate_url("https://github.com/menukahansda/task-management-system")
+        == EXPECTED
+    )
 
 
 def test_valid_url_with_query_and_fragment():
     assert (
         validate_url(
-            "https://github.com/menukahansda/ask-my-repo?tab=readme#section"
+            "https://github.com/menukahansda/task-management-system"
+            "?tab=readme#section"
         )
         == EXPECTED
     )
-    
-    
+
+
 def test_root_only():
     assert validate_url("https://github.com/") is None
 
@@ -37,7 +43,7 @@ def test_root_only():
 def test_non_github_domain():
     assert (
         validate_url(
-            "https://www.youtube.com/watch?v=EbI74XqaJ_0&list=RD9aED02XuLwo&index=2"
+            "https://www.youtube.com/watch?v=EbI74XqaJ_0" "&list=RD9aED02XuLwo&index=2"
         )
         is None
     )
@@ -56,19 +62,30 @@ def test_none_input():
 
 
 def test_whitespace_padding():
-    assert validate_url("  https://github.com/menukahansda/ask-my-repo  ") == EXPECTED
+    assert (
+        validate_url("  https://github.com/menukahansda/task-management-system  ")
+        == EXPECTED
+    )
 
 
 def test_already_has_git_suffix():
-    assert validate_url("https://github.com/menukahansda/ask-my-repo.git") == EXPECTED
+    assert (
+        validate_url("https://github.com/menukahansda/task-management-system.git")
+        == EXPECTED
+    )
 
 
 def test_uppercase_domain():
-    assert validate_url("https://GITHUB.COM/menukahansda/ask-my-repo") == EXPECTED
+    assert (
+        validate_url("https://GITHUB.COM/menukahansda/task-management-system")
+        == EXPECTED
+    )
 
 
 def test_ssh_style_url_rejected():
-    assert validate_url("git@github.com:menukahansda/ask-my-repo.git") is None
+    assert (
+        validate_url("git@github.com:menukahansda/task-management-system.git") is None
+    )
 
 
 def test_gist_subdomain_rejected():
@@ -76,8 +93,14 @@ def test_gist_subdomain_rejected():
 
 
 def test_double_slash_in_path():
-    assert validate_url("https://github.com/menukahansda//ask-my-repo") == EXPECTED
+    assert (
+        validate_url("https://github.com/menukahansda//task-management-system")
+        == EXPECTED
+    )
 
 
 def test_http_scheme_accepted():
-    assert validate_url("http://github.com/menukahansda/ask-my-repo") == EXPECTED
+    assert (
+        validate_url("http://github.com/menukahansda/task-management-system")
+        == EXPECTED
+    )
