fc9dbf10b11808354654be569d2b5557e23b48ec
menukahansda <menukahansda199@gmail.com>
2026-08-20 17:55:29 +0530

    chore: configure uvicorn reload directory

--- backend/app/main.py
+++ backend/app/main.py
@@ -85,4 +85,4 @@ def cleanup():
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", reload=True, host="127.0.0.1", port=PORT)
+    uvicorn.run("app.main:app", reload=True, reload_dirs=["app"], host="127.0.0.1", port=PORT)
